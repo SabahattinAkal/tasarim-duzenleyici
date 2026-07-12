75e1fe35ee37978b5ead312719642dee0a3334ef
Sabahattin Akal <109273966+SabahattinAkal@users.noreply.github.com>
2026-07-12 18:38:20 +0300

    Update giderpusulasi.xml.js

--- giderpusulasi.xml.js
+++ giderpusulasi.xml.js
@@ -1,1 +1,38 @@
-
+<GiderPusulasi xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<Seri>A</Seri>
+	<SiraNo>10040</SiraNo>
+	<Tarih>10.07.2026</Tarih>
+	<Sehir>34</Sehir>
+	<Gonderici>
+		<VKNorTCKN>3230512384</VKNorTCKN>
+		<UnvanorAdSoyad>EDM BİLİŞİM SİSTEMLERİ VE DANIŞMANLIK HİZMETLERİ ANONİM ŞİRKETİ</UnvanorAdSoyad>
+		<Adres>Cumhuriyet Mah. Yeni Yol 1 Sok. Bomonti Business Center Kat:7/ D:36</Adres>
+		<Mail>bilgi@edmbilisim.com.tr</Mail>
+		<VergiDairesi>ŞİŞLİ VERGİ DAİRESİ MÜD.</VergiDairesi>
+	</Gonderici>
+	<Alici>
+		<AdSoyad>ELİF EKİNCİ</AdSoyad>
+		<TCKN>23396596148</TCKN>
+		<Adres>-</Adres>
+		<PlakaNo/>
+		<Mail/>
+	</Alici>
+	<Satir>
+		<SatirBilgi>
+			<Mahiyet>OFİS DESTEK PERSONELİ YEMEK BEDELİ</Mahiyet>
+			<Cinsi>5 GÜN</Cinsi>
+			<Adet>1 Adet</Adet>
+			<Fiyat>1840,8888 TRY</Fiyat>
+			<Tutar>1840,89 TRY</Tutar>
+		</SatirBilgi>
+	</Satir>
+	<Toplam>
+		<Toplam>1840.89</Toplam>
+		<GelirVergisiOran>10</GelirVergisiOran>
+		<GelirVergisiTutar>184.09</GelirVergisiTutar>
+		<DigerKesintiler>0</DigerKesintiler>
+		<KesintiToplami>184.09</KesintiToplami>
+		<KDV>0</KDV>
+		<ToolamOdenecekTutar>1656.8</ToolamOdenecekTutar>
+	</Toplam>
+</GiderPusulasi>
